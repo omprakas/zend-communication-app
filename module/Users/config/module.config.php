b42fce2a42970fbf4dd3a8531a56bf250c0caccb
petrero <petrero@gmail.com>
2014-01-09 18:20:28 +0200

    make necessary routes for upload resource and create index view

--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -5,7 +5,8 @@ return array(
             'Users\Controller\Index' => 'Users\Controller\IndexController',
             'Users\Controller\Register' => 'Users\Controller\RegisterController',
             'Users\Controller\Login' => 'Users\Controller\LoginController',
-            'Users\Controller\UserManager' => 'Users\Controller\UserManagerController'
+            'Users\Controller\UserManager' => 'Users\Controller\UserManagerController',
+            'Users\Controller\UploadManager' => 'Users\Controller\UploadManagerController'
         ),
     ),
     'router' => array(
@@ -69,6 +70,20 @@ return array(
                             )
                         )
                     ),
+                    'upload-manager' => array(
+                        'type' => 'Segment',
+                        'options' => array(
+                            'route' => '/upload-manager[/:action[/:id]]',
+                            'constraints' => array(
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id' => '[a-zA-Z0-9_-]*'                              
+                            ),
+                            'defaults' => array(
+                                'controller' => 'Users\Controller\UploadManager',
+                                'action' => 'index'
+                            )
+                        )
+                    )
                 ),
             ),
         ),
